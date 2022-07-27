fd33d79a606c486e780d16fa5ca695e7d8e807ca
Nitya886 <66220305+Nitya886@users.noreply.github.com>
2022-07-27 12:24:18 +0530

    Add files via upload

---
+++ (5)Q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter n.\n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("MySirG\n");
+    }
+    return 0;
+}
+
---
+++ (5)Q5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter n.\n");
+    scanf("%d",&n);
+    for(i=(2*n);i>=1;i--)
+    {
+        if(i%2!=0)
+        printf("%d\n",i);
+    }
+    return 0;
+}
+
+
+
+
